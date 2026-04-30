e7e6094d4d0f44d5daeb7b2dbf201161eca17c49
tanaya2005 <tanayaitis@gmail.com>
2026-04-30 18:05:02 +0530

    Revert "copying project from the private hackathon repository"
    This reverts commit d36faeb82bba7754c1696de60717dd81d36b3460.

--- server/routes/analyticsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getViolationAnalytics } = require('../controllers/analyticsController');
-const { protect } = require('../middleware/auth');
-
-router.get('/violations', protect, getViolationAnalytics);
-
-module.exports = router;
